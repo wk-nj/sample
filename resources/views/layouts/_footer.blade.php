f5c3f4dd8f20e3af9529699ae62b4c9697a648b7
wk-nj <15380314863@163.com>
2018-05-08 16:26:39 +0800

    procfile for Heroku

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,8 +1,8 @@
 <div class="col-md-12">
   <footer class="footer">
     <small class="slogon">
-      <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://laravel-china.org/courses">
+      <img class="brand-icon" src="./public/bi.ico">
+      <a href="#">
         刻意练习，每日精进
       </a>
     </small>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -21,9 +21,9 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
+      <h1>朋友你好</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+        欢迎来到sample
       </p>
       <p>
         一切，将从这里开始。
